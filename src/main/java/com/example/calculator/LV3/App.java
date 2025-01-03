cc658333429752e2079f65c8e2fdc66c5cf2d8e0
RyuSungHyeon990604 <shryoo990604@gmail.com>
2025-01-03 15:44:32 +0900

    수정 : LV3 / 중복코드 함수로 작성

--- src/main/java/com/example/calculator/LV3/App.java
+++ src/main/java/com/example/calculator/LV3/App.java
@@ -1,93 +1,131 @@
 package com.example.calculator.LV3;
 
-import com.example.calculator.Validator;
-
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Collection;
 import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 //Lv 1에서 구현한 App 클래스의 main 메서드에 Calculator 클래스가 활용될 수 있도록 수정
 public class App {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Validator v = new Validator();
+    private Scanner sc;
+    private OperatorType op = null;
+    private ArithmeticCalculator<Double> calc;
+    public App(){
+        sc = new Scanner(System.in);
+        calc = new ArithmeticCalculator<>();
+    }
+    public void run(){
         double num1 =0 ,num2 = 0;
-        OperatorType op = null;
-        String more = "go";
-        char yn = 'y';
         double res=0.0;
-        ArithmeticCalculator<Double> calc = new ArithmeticCalculator<>();
-        //반복문을 사용하되, 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정하기
-        while(!more.equals("exit")) {
-            String input="";
-            //숫자 입력받기
-            System.out.print("첫 번째 숫자를 입력하세요 : ");
-            input = sc.next();
-            while(!v.isNumber(input)) {
-                System.out.println("숫자를 입력해주세요");
-                System.out.print("첫 번째 숫자를 입력하세요 : ");
-                input = sc.next();
-            }
-            num1 = Double.parseDouble(input);
+        //숫자 입력받기
+        num1 = getInput("첫 번째 숫자를 입력하세요 : ");
+        num2 = getInput("두 번째 숫자를 입력하세요 : ");
 
-            System.out.print("두 번째 숫자를 입력하세요 : ");
-            input = sc.next();
-            while(!v.isNumber(input)) {
-                System.out.println("숫자를 입력해주세요");
-                System.out.print("두 번째 숫자를 입력하세요 : ");
-                input = sc.next();
-            }
-            num2 = Double.parseDouble(input);
-
-            try{
-                //사칙연산 기호(➕,➖,✖️,➗)를 입력받기
-                System.out.print("사칙연산 기호(+,-,*,/)를 입력 해주세요.");
-                op = OperatorType.getOperatorType(sc.next().charAt(0));
-                calc.setOperation(op.getOperation());
-                res = calc.calculate(num1,num2);
-
-                System.out.printf("%.1f %c %.1f = %.3f\n",num1, op.getLabel() ,num2, res);
-            }catch (Exception e) {
-                //연산 오류가 발생할 경우 해당 오류에 대한 내용을 정제하여 출력합니다.
-                System.out.println(e.getMessage());
-                continue;
-            }
-
-            System.out.println("결과를 저장하시겠습니까? ( Y / N )");
-            yn = sc.next().charAt(0);
-            if(yn == 'y' || yn == 'Y') {
-                calc.saveResults(res);
-            }
-
-
-            if(!calc.getResults().isEmpty()) {
-                System.out.println("가장 오래된 결과를 삭제하시겠습니까? ( Y / N )");
-                System.out.println(calc.getResults());
-                yn = sc.next().charAt(0);
-                if((yn == 'Y' || yn == 'y')){
-                    calc.remove();
-                }
-            }
-
-            System.out.println("검색기능을 사용하시겠습니까? ( Y / N )");
-            yn = sc.next().charAt(0);
-            if((yn == 'Y' || yn == 'y')){
-                System.out.print("어떤 수보다 큰 결과를 원하시나요? : ");
-                input = sc.next();
-                while(!v.isNumber(input)) {
-                    System.out.print("숫자를 입력해주세요 : ");
-                    input = sc.next();
-                }
-                double search = Double.parseDouble(input);
-                List<Double> list = calc.search(search);
-                if(list.isEmpty()) {
-                    System.out.println(input + "보다 큰 계산 결과가 없습니다.");
-                }else System.out.println(list);
-            }
-
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            more = sc.next();
+        try{
+            //사칙연산 기호(➕,➖,✖️,➗)를 입력받기
+            System.out.print("사칙연산 기호(+,-,*,/)를 입력 해주세요.");
+            op = OperatorType.getOperatorType(sc.next().charAt(0));
+            //set operation
+            calc.setOperation(op.getOperation());
+            res = calc.calculate(num1,num2);
+            System.out.printf("%.1f %c %.1f = %.5f\n",num1, op.getLabel() ,num2, round(res, 5));
+        }catch (Exception e) {
+            //연산 오류가 발생할 경우 해당 오류에 대한 내용을 정제하여 출력합니다.
+            System.out.println(e.getMessage());
+            return;
         }
 
+        if(yn("결과를 저장하시겠습니까? ( Y / N )","y")) {
+            calc.saveResults(res);
+        }
+
+        Queue<Double> results = calc.getResults();
+        if(results.isEmpty()) {
+            System.out.println("저장된 결과가 없습니다.");
+        }else{
+            printCollection(results);
+
+            if(yn("\n가장 오래된 결과를 삭제하시겠습니까? ( Y / N )","y")){
+                Double remove = calc.remove();
+                System.out.println(new BigDecimal(remove).setScale(3,RoundingMode.HALF_UP)+" 을 삭제했습니다.");
+            }
+
+            results = calc.getResults();
+            if(results.isEmpty()) {
+                System.out.println("저장된 결과가 없습니다.");
+            }else{
+                printCollection(results);
+            }
+        }
+
+        if(!results.isEmpty()) {
+            if(yn("\n검색기능을 사용하시겠습니까? ( Y / N )","y")){
+                double search = getInput("어떤 수보다 큰 결과를 원하시나요? : ");
+                List<Double> list = calc.search(search);
+                if(list.isEmpty()) {
+                    System.out.println("\n"+ search + "보다 큰 계산 결과가 없습니다.");
+                }else {
+                    printCollection(list);
+                }
+            }
+        }
+    }
+    private double getInput(String str) {
+        System.out.print(str);
+        String input = sc.next();
+        while(!isNumber(input)) {
+            System.out.print("숫자를 입력해주세요");
+            input = sc.next();
+        }
+        return Double.parseDouble(input);
+    }
+    private boolean yn(String str,String ifTrue) {
+        System.out.println(str);
+        String input = sc.next().toLowerCase();
+        return input.equals(ifTrue);
+    }
+
+    public boolean isNumber(String input) {
+        try{
+            Double.parseDouble(input);
+            return true;
+        }catch(NumberFormatException e){
+            return false;
+        }
+    }
+
+    public void printCollection(Collection<?> obj) {
+        if(obj == null){
+            System.out.println("obj is null");
+            return;
+        }
+        if(obj.isEmpty()){
+            System.out.println("obj is empty");
+            return;
+        }
+        for (Object o : obj) {
+            if(o instanceof Number) {
+                System.out.print(round((double)o,5)+" ");
+            }else
+                System.out.println(o+" ");
+        }
+    }
+
+    public BigDecimal round(double value, int places) {
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd;
+    }
+
+    public static void main(String[] args) {
+        App app  =new App();
+        Scanner sc = new Scanner(System.in);
+        do{
+            app.run();
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+        }while(!sc.next().equals("exit"));
+        System.out.println("계산기를 종료합니다.");
     }
 }
